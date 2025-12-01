2d30a58220157ecf77a51fa61be1df1233837df7
HP23005 <hp23005@ues.edu.sv>
2025-12-01 12:07:05 -0600

    Create CorsConfig.java

---
+++ backend/src/main/java/com/reservas/backend/config/CorsConfig.java
@@ -0,0 +1,16 @@
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://sistema-reservas-canchas-frontend.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
